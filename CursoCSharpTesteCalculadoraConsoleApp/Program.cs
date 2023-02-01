47410e393aaaa2d337f3329b00113211e6b46c51
instrutor <instrutor@CursoCSharp>
2023-02-01 10:45:48 +0000

    feat: criou uma classe calculadora e realizou tratamento de erro

--- CursoCSharpTesteCalculadoraConsoleApp/Program.cs
+++ CursoCSharpTesteCalculadoraConsoleApp/Program.cs
@@ -1,57 +1,18 @@
-﻿Console.WriteLine("Seja Bem Vindo a Calculadora");
+﻿using CursoCSharpTesteCalculadoraConsoleApp;
 
-do
+Console.WriteLine("Seja Bem Vindo a Calculadora");
+
+try
+{
+    Calculadora calculadora = new Calculadora();
+    calculadora.Executar();
+}
+catch (Exception ex)
 {
     Console.Clear();
-    Console.WriteLine("Digite o primeiro valor:");
-    string? primeiroValorString = Console.ReadLine();
-    double primeiroValor = Convert.ToDouble(primeiroValorString);
+    Console.WriteLine("Deu erro, nao consegui realizar o processo!!!");
+}
 
-    Console.WriteLine("Digite o segundo valor:");
-    string? segundoValorString = Console.ReadLine();
-    double segundoValor = Convert.ToDouble(segundoValorString);
-
-    Console.WriteLine("Selecione a operacao:");
-    Console.WriteLine("+");
-    Console.WriteLine("-");
-    Console.WriteLine("*");
-    Console.WriteLine("/");
-
-    var operacaoSelecionada = Console.ReadLine();
-
-    double? resultadoOperacao;
-    switch (operacaoSelecionada)
-    {
-        case "+":
-            resultadoOperacao = primeiroValor + segundoValor;
-            break;
-        case "-":
-            resultadoOperacao = primeiroValor - segundoValor;
-            break;
-        case "*":
-            resultadoOperacao = primeiroValor * segundoValor;
-            break;
-        case "/":
-            resultadoOperacao = primeiroValor / segundoValor;
-            break;
-        default:
-            resultadoOperacao = null;
-            break;
-    }
-
-    Console.WriteLine();
-    var mensagem = resultadoOperacao.HasValue ? $"Resultado da operacao: {resultadoOperacao}" : "Operacao invalida!";
-    Console.WriteLine(mensagem);
-
-    //if (resultadoOperacao.HasValue)
-    //    Console.WriteLine($"Resultado da operacao: {resultadoOperacao}");
-    //else
-    //    Console.WriteLine("Operacao invalida!");
-
-    Console.WriteLine();
-    Console.WriteLine("Deseja realizar outra operacao?");
-    Console.WriteLine("(S) Sim (N) Nao");
-} while (Console.ReadKey().Key == ConsoleKey.S);
 
 Console.WriteLine();
 Console.WriteLine("Ate logo!!");
